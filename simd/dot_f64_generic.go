d9cf21708f9a51d0d41006ccc104b4d337009ad4
openfluke-dev <openfluke@hotmail.com>
2026-08-26 23:16:27 +1000

    found bug lol

--- simd/dot_f64_generic.go
+++ simd/dot_f64_generic.go
@@ -2,10 +2,17 @@
 
 package simd
 
+import "unsafe"
+
 func dotTileF64Simd(x, w *float64, n int, prev float64) float64 {
+	if n <= 0 {
+		return prev
+	}
+	xs := unsafe.Slice(x, n)
+	ws := unsafe.Slice(w, n)
 	sum := prev
 	for i := 0; i < n; i++ {
-		sum += x[i] * w[i]
+		sum += xs[i] * ws[i]
 	}
 	return sum
 }
